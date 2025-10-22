aeed8496cd359318352a6c979f7ea9fa9d054666
siudepzai <nguyenthanhtuan01081996@gmail.com>
2025-10-22 18:53:05 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Thuat_toan_DES
+{
+    partial class GUI_DES
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lab_Ban_Ro = new System.Windows.Forms.Label();
+            this.lab_Key = new System.Windows.Forms.Label();
+            this.lab_enrypt = new System.Windows.Forms.Label();
+            this.lab_Decrypt = new System.Windows.Forms.Label();
+            this.txt_cipher = new System.Windows.Forms.TextBox();
+            this.txt_plaint = new System.Windows.Forms.TextBox();
+            this.txt_Plain_Text = new System.Windows.Forms.TextBox();
+            this.txt_Key = new System.Windows.Forms.TextBox();
+            this.btn_Ecrypt = new System.Windows.Forms.Button();
+            this.btn_Decrypt = new System.Windows.Forms.Button();
+            this.txt_LogEncrypt = new System.Windows.Forms.TextBox();
+            this.txt_LogDecrypt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(411, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(293, 55);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Thuật toán DES";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lab_Ban_Ro
+            // 
+            this.lab_Ban_Ro.AutoSize = true;
+            this.lab_Ban_Ro.Location = new System.Drawing.Point(130, 101);
+            this.lab_Ban_Ro.Name = "lab_Ban_Ro";
+            this.lab_Ban_Ro.Size = new System.Drawing.Size(243, 22);
+            this.lab_Ban_Ro.TabIndex = 8;
+            this.lab_Ban_Ro.Text = "Nhập vào đoạn muốn mã hóa:";
+            // 
+            // lab_Key
+            // 
+            this.lab_Key.AutoSize = true;
+            this.lab_Key.Location = new System.Drawing.Point(234, 158);
+            this.lab_Key.Name = "lab_Key";
+            this.lab_Key.Size = new System.Drawing.Size(139, 22);
+            this.lab_Key.TabIndex = 9;
+            this.lab_Key.Text = "Nhập vào khóa :";
+            // 
+            // lab_enrypt
+            // 
+            this.lab_enrypt.AutoSize = true;
+            this.lab_enrypt.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_enrypt.Location = new System.Drawing.Point(173, 208);
+            this.lab_enrypt.Name = "lab_enrypt";
+            this.lab_enrypt.Size = new System.Drawing.Size(159, 33);
+            this.lab_enrypt.TabIndex = 10;
+            this.lab_enrypt.Text = "Bảng mã hóa";
+            // 
+            // lab_Decrypt
+            // 
+            this.lab_Decrypt.AutoSize = true;
+            this.lab_Decrypt.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_Decrypt.Location = new System.Drawing.Point(771, 208);
+            this.lab_Decrypt.Name = "lab_Decrypt";
+            this.lab_Decrypt.Size = new System.Drawing.Size(160, 33);
+            this.lab_Decrypt.TabIndex = 0;
+            this.lab_Decrypt.Text = "Bảng giải mã";
+            // 
+            // txt_cipher
+            // 
+            this.txt_cipher.Location = new System.Drawing.Point(12, 495);
+            this.txt_cipher.Multiline = true;
+            this.txt_cipher.Name = "txt_cipher";
+            this.txt_cipher.ReadOnly = true;
+            this.txt_cipher.Size = new System.Drawing.Size(485, 39);
+            this.txt_cipher.TabIndex = 3;
+            // 
+            // txt_plaint
+            // 
+            this.txt_plaint.Location = new System.Drawing.Point(593, 495);
+            this.txt_plaint.Multiline = true;
+            this.txt_plaint.Name = "txt_plaint";
+            this.txt_plaint.ReadOnly = true;
+            this.txt_plaint.Size = new System.Drawing.Size(485, 39);
+            this.txt_plaint.TabIndex = 4;
+            // 
+            // txt_Plain_Text
+            // 
+            this.txt_Plain_Text.Location = new System.Drawing.Point(379, 93);
+            this.txt_Plain_Text.Name = "txt_Plain_Text";
+            this.txt_Plain_Text.Size = new System.Drawing.Size(434, 30);
+            this.txt_Plain_Text.TabIndex = 1;
+            // 
+            // txt_Key
+            // 
+            this.txt_Key.Location = new System.Drawing.Point(379, 150);
+            this.txt_Key.Name = "txt_Key";
+            this.txt_Key.Size = new System.Drawing.Size(434, 30);
+            this.txt_Key.TabIndex = 2;
+            // 
+            // btn_Ecrypt
+            // 
+            this.btn_Ecrypt.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Ecrypt.Location = new System.Drawing.Point(179, 556);
+            this.btn_Ecrypt.Name = "btn_Ecrypt";
+            this.btn_Ecrypt.Size = new System.Drawing.Size(153, 50);
+            this.btn_Ecrypt.TabIndex = 5;
+            this.btn_Ecrypt.Text = "Encrypt";
+            this.btn_Ecrypt.UseVisualStyleBackColor = true;
+            // 
+            // btn_Decrypt
+            // 
+            this.btn_Decrypt.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Decrypt.Location = new System.Drawing.Point(777, 556);
+            this.btn_Decrypt.Name = "btn_Decrypt";
+            this.btn_Decrypt.Size = new System.Drawing.Size(154, 50);
+            this.btn_Decrypt.TabIndex = 6;
+            this.btn_Decrypt.Text = "Decrypt";
+            this.btn_Decrypt.UseVisualStyleBackColor = true;
+            // 
+            // txt_LogEncrypt
+            // 
+            this.txt_LogEncrypt.Location = new System.Drawing.Point(12, 257);
+            this.txt_LogEncrypt.Multiline = true;
+            this.txt_LogEncrypt.Name = "txt_LogEncrypt";
+            this.txt_LogEncrypt.ReadOnly = true;
+            this.txt_LogEncrypt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_LogEncrypt.Size = new System.Drawing.Size(485, 221);
+            this.txt_LogEncrypt.TabIndex = 3;
+            // 
+            // txt_LogDecrypt
+            // 
+            this.txt_LogDecrypt.Location = new System.Drawing.Point(593, 257);
+            this.txt_LogDecrypt.Multiline = true;
+            this.txt_LogDecrypt.Name = "txt_LogDecrypt";
+            this.txt_LogDecrypt.ReadOnly = true;
+            this.txt_LogDecrypt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_LogDecrypt.Size = new System.Drawing.Size(485, 221);
+            this.txt_LogDecrypt.TabIndex = 4;
+            // 
+            // GUI_DES
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1100, 619);
+            this.Controls.Add(this.btn_Decrypt);
+            this.Controls.Add(this.btn_Ecrypt);
+            this.Controls.Add(this.txt_Key);
+            this.Controls.Add(this.txt_Plain_Text);
+            this.Controls.Add(this.txt_LogDecrypt);
+            this.Controls.Add(this.txt_LogEncrypt);
+            this.Controls.Add(this.txt_plaint);
+            this.Controls.Add(this.txt_cipher);
+            this.Controls.Add(this.lab_Decrypt);
+            this.Controls.Add(this.lab_enrypt);
+            this.Controls.Add(this.lab_Key);
+            this.Controls.Add(this.lab_Ban_Ro);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "GUI_DES";
+            this.Text = "Giao diện thuật toán DES";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lab_Ban_Ro;
+        private System.Windows.Forms.Label lab_Key;
+        private System.Windows.Forms.Label lab_enrypt;
+        private System.Windows.Forms.Label lab_Decrypt;
+        private System.Windows.Forms.TextBox txt_cipher;
+        private System.Windows.Forms.TextBox txt_plaint;
+        private System.Windows.Forms.TextBox txt_Plain_Text;
+        private System.Windows.Forms.TextBox txt_Key;
+        private System.Windows.Forms.Button btn_Ecrypt;
+        private System.Windows.Forms.Button btn_Decrypt;
+        private System.Windows.Forms.TextBox txt_LogEncrypt;
+        private System.Windows.Forms.TextBox txt_LogDecrypt;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Thuat_toan_DES
+{
+    public partial class GUI_DES : Form
+    {
+        public GUI_DES()
+        {
+            InitializeComponent();
+            // Gán sự kiện cho nút
+            btn_Ecrypt.Click += Btn_Ecrypt_Click;
+            btn_Decrypt.Click += Btn_Decrypt_Click;
+        }
+        private void Btn_Ecrypt_Click(object sender, EventArgs e)
+        {
+            string plainText = txt_Plain_Text.Text;
+            string keyText = txt_Key.Text;
+
+            if (string.IsNullOrEmpty(plainText))
+            {
+                MessageBox.Show("Vui lòng nhập bản rõ!");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(keyText))
+            {
+                MessageBox.Show("Vui lòng nhập khóa!");
+                return;
+            }
+
+            // Khóa 8 byte
+            if (keyText.Length > 8)
+                keyText = keyText.Substring(0, 8);
+            else if (keyText.Length < 8)
+                keyText = keyText.PadRight(8, '0');
+
+            byte[] keyBytes = Encoding.ASCII.GetBytes(keyText);
+            Thuat_Toan_DES des = new Thuat_Toan_DES(keyBytes);
+            des.LogAction = msg =>
+            {
+                txt_LogEncrypt.Invoke(new Action(() =>
+                {
+                    txt_LogEncrypt.AppendText(msg + Environment.NewLine);
+                }));
+            };
+
+            byte[] plainBytes = Encoding.ASCII.GetBytes(plainText);
+
+            // --- Thêm PKCS#7 padding ---
+            int padLen = 8 - (plainBytes.Length % 8);
+            if (padLen == 0) padLen = 8;
+            byte[] padded = new byte[plainBytes.Length + padLen];
+            Array.Copy(plainBytes, padded, plainBytes.Length);
+            for (int i = plainBytes.Length; i < padded.Length; i++)
+                padded[i] = (byte)padLen;
+            // ----------------------------
+
+            List<byte> encryptedAll = new List<byte>();
+
+            for (int i = 0; i < padded.Length; i += 8)
+            {
+                byte[] block = new byte[8];
+                Array.Copy(padded, i, block, 0, 8);
+                byte[] encBlock = des.encrypt_block(block);
+                encryptedAll.AddRange(encBlock);
+            }
+
+            txt_cipher.Text = BitConverter.ToString(encryptedAll.ToArray()).Replace("-", " ");
+
+        }
+
+
+        private void Btn_Decrypt_Click(object sender, EventArgs e)
+        {
+            string cipherText = txt_cipher.Text.Trim();
+            string keyText = txt_Key.Text;
+
+            if (string.IsNullOrEmpty(cipherText))
+            {
+                MessageBox.Show("Không có dữ liệu mã hóa để giải mã!");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(keyText))
+            {
+                MessageBox.Show("Vui lòng nhập khóa!");
+                return;
+            }
+
+            if (keyText.Length > 8)
+                keyText = keyText.Substring(0, 8);
+            else if (keyText.Length < 8)
+                keyText = keyText.PadRight(8, '0');
+
+            byte[] keyBytes = Encoding.ASCII.GetBytes(keyText);
+            Thuat_Toan_DES des = new Thuat_Toan_DES(keyBytes);
+            des.LogAction = msg =>
+            {
+                txt_LogDecrypt.Invoke(new Action(() =>
+                {
+                    txt_LogDecrypt.AppendText(msg + Environment.NewLine);
+                }));
+            };
+
+            byte[] cipherBytes = cipherText.Split(' ')
+                                           .Where(x => !string.IsNullOrWhiteSpace(x))
+                                           .Select(x => Convert.ToByte(x, 16))
+                                           .ToArray();
+
+            List<byte> decryptedAll = new List<byte>();
+
+            for (int i = 0; i < cipherBytes.Length; i += 8)
+            {
+                byte[] block = new byte[8];
+                Array.Copy(cipherBytes, i, block, 0, 8);
+                byte[] decBlock = des.decrypted_block(block);
+                decryptedAll.AddRange(decBlock);
+            }
+
+            // --- Bỏ padding ---
+            byte padVal = decryptedAll[decryptedAll.Count - 1];
+            if (padVal > 0 && padVal <= 8)
+                decryptedAll.RemoveRange(decryptedAll.Count - padVal, padVal);
+
+            string plainOut = Encoding.ASCII.GetString(decryptedAll.ToArray());
+            txt_plaint.Text = plainOut;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
